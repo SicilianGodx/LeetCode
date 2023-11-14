a2047705cf9f7e0a375906570447bea86c53b204
markw <mwulfert176@gmail.com>
2023-11-14 13:54:04 -0800

    First functional #88

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,27 +6,41 @@ LeetCode::LeetCode(){}
 //88. Merge Sorted Array - easy
 void LeetCode::merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n){
     std::vector<int> ans;
-    std::reverse(nums1.begin(), nums1.end());
-    std::reverse(nums2.begin(), nums2.end());
-    for(int i = 0; i < m + n; i++){
-        if (nums2.empty() == true){
-            ans.push_back(nums1.back());
-            nums1.pop_back();
-            continue;
-        }
-        if (nums1.back() == 0){
-            ans.push_back(nums2.back());
-            nums2.pop_back();
-            continue;
-        }
-        if (nums1.back() < nums2.back()){
-            ans.push_back(nums1.back());
-            nums1.pop_back();
+    std::vector<int>::iterator itr1 = nums1.begin();
+    std::vector<int>::iterator itr2 = nums2.begin();
+    int track1 = 0;
+    int track2 = 0;
+
+    for (int i = 0; ((track1 < m) && (track2 < n)); i++){
+        std::cout << "what?" << "\n";
+        if (*itr1 < *itr2){
+            ans.push_back(*itr1);
+            itr1++;
+            track1++;
+            std::cout << track1 << "\n";
         }
         else{
-            ans.push_back(nums2.back());
-            nums2.pop_back();
+            ans.push_back(*itr2);
+            itr2++;
+            track2++;
         }
+    }
+
+    if (track1 == m){
+        std::cout << "here!" << "\n";
+        for (int j = track2; j < (n-1); j++){
+            ans.push_back(*itr2);
+            itr2++;
+        }
+        ans.push_back(*itr2);
+    }
+
+    else{
+        for (int j = track1; j < (m-1); j++){
+            ans.push_back(*itr1);
+            itr1++;
+        }
+        ans.push_back(*itr1);
     }
     nums1 = ans;
 }
--- Main.cpp
+++ Main.cpp
@@ -4,20 +4,17 @@ int main(){
     
     LeetCode LeetCode;
 
-    std::vector<int> vec1{2,0};
-    std::vector<int> vec2{1};
-    int nums1 = 2;
-    int nums2 = 1;
+    std::vector<int> nums1{1,2,3,0,0,0};
+    std::vector<int> nums2{};
+    int m = 3;
+    int n = 0;
 
-    LeetCode.merge(vec1, nums1, vec2, nums2);
+    LeetCode.merge(nums1, m, nums2, n);
 
-    std::reverse(vec1.begin(), vec1.end());
-
-    for (int i = 0; i < nums1 + nums2; i++){
-        std::cout << vec1.back() << " ";
-        vec1.pop_back();
+    for (std::vector<int>::iterator itr = nums1.begin(); itr != nums1.end(); itr++){
+        std::cout << *itr << " ";
     }
-    std::cout << "\n";
+    std::cout << std::endl;
 
 
     return 0;
